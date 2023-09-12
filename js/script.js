a6f89e876e5ac3310a5a244b5de6c06f3435143b
jms8540 <j01085402702@gmail.com>
2023-09-12 16:59:01 +0900

    sub_menu_item을 main_menu에서 꺼낸후 실행. (미완)

--- js/script.js
+++ js/script.js
@@ -21,30 +21,37 @@ addBt.addEventListener("click", () => {
   addMenu_addBt.setAttribute("src", "./img/menuItem_addIcon.png");
   addMenu_addBt.classList.add("addMenu_addBt_img");
   addMenu.appendChild(addMenu_addBt);  // addMenu_addBt_img
+  // main_menu 추가 버튼
 
-  const sub_menu = document.querySelectorAll(".addMenu_addBt_img");
-  const sub_menuArr = [...sub_menu];
-  sub_menuArr.forEach((e, idx)=>{
-    e.addEventListener("click", ()=>{
-      const sub_item = document.createElement("div");
-      sub_menu[idx].style.height = "50px";
-      setTimeout(() => (sub_menu[idx].style.height = `${40}px`), 100);
-      sub_item.classList.add("sub_item");
-      sub_menu[idx].parentNode.appendChild(sub_item);
-      const subMenu_addBt = document.querySelectorAll(".addMenu_addBt_img");
-      const subMenu_title = document.querySelector(".mainItem_title");
-      subMenu_addBt.forEach(e =>{
-        e.addEventListener("click",(el) =>{
+  // const sub_menu = document.querySelectorAll(".addMenu_addBt_img");
+  // const sub_menuArr = [...sub_menu];
+  // sub_menuArr.forEach((e, idx)=>{
+  //   e.addEventListener("click", ()=>{
+  //     const sub_item = document.createElement("div");
+  //     sub_menu[idx].style.height = "50px";
+  //     setTimeout(() => (sub_menu[idx].style.height = `${40}px`), 100);
+  //     sub_item.classList.add("sub_item");
+  //     sub_menu[idx].parentNode.appendChild(sub_item);
+  //     const subMenu_addBt = document.querySelectorAll(".addMenu_addBt_img");
+  //     const subMenu_title = document.querySelector(".mainItem_title");
+  //     subMenu_addBt.forEach(e =>{
+  //       e.addEventListener("click",(el) =>{
           
-        })
-      })
+  //       })
+  //     })
       
-    });
-  });
-}); // main_menu 추가 버튼
+  //   });
+  // });
+  // sub_menu 추가 버튼
+});
 
-// sub_menu 추가 버튼
-
+const sub_menu = document.querySelector(".addMenu_addBt_img");
+const subMenu_title = document.querySelector(".mainItem_title");
+sub_menu.addEventListener("click", ()=>{
+  const sub_menu_item = document.createElement("div");
+  
+  subMenu_title
+});
 
 // subMenu_addBt.addEventListener("click", ()=>{
 //   const addSubmenu_item = document.createElement("div");
